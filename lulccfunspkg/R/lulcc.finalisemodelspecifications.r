9a1c2b073fb7837861ff421316908cb25cb70c16
mmyrte <24587121+mmyrte@users.noreply.github.com>
2025-01-31 17:19:15 +0100

    linter complaints

--- lulccfunspkg/R/lulcc.finalisemodelspecifications.r
+++ lulccfunspkg/R/lulcc.finalisemodelspecifications.r
@@ -7,37 +7,53 @@
 #' @author Ben Black
 #' @export
 
-lulcc.finalisemodelspecifications <- function(model_specs_path, param_grid_path) {
+lulcc.finalisemodelspecifications <- function(model_specs_path, param_grid_path, data_periods) {
   # Load model specifications
-  model_specs <- read_excel(model_specs_path)
+  model_specs <- readxl::read_excel(model_specs_path)
 
   # filter for completed model specifcations
   model_specs <- model_specs[model_specs$Modelling_completed == "Y", ]
 
   # Use prompts to get specifications as input from user
-  Model_type <- readline(prompt = paste0("Choose model type from: ", paste(unique(model_specs$Model_type), sep = "or"), ", Enter type: "))
-  model_scale <- readline(prompt = paste0("Choose model scale from: ", paste(unique(model_specs$model_scale), sep = "or"), ", Enter scale: "))
-  Feature_selection_employed <- readline(prompt = "Use transitions datasets that have undergone feature selection (TRUE or FALSE): ")
-  Balance_adjustment <- if (Model_type == "rf") {
+  model_type <- readline(prompt = paste0(
+    "Choose model type from: ",
+    paste(unique(model_specs$Model_type), sep = "or"),
+    ", Enter type: "
+  ))
+  model_scale <- readline(prompt = paste0(
+    "Choose model scale from: ",
+    paste(unique(model_specs$model_scale), sep = "or"),
+    ", Enter scale: "
+  ))
+  feature_selection_employed <- readline(
+    prompt = "Use transitions datasets that have undergone feature selection (TRUE or FALSE): "
+  )
+  balance_adjustment <- if (model_type == "rf") {
     readline(prompt = "Use Tree-wise downsampling to address class imbalance: ")
   } else {
     "FALSE"
   }
 
   # Subset model specifications by user inputs
-  Predict_model_specs <- model_specs[model_specs$Model_type == Model_type &
-    model_specs$model_scale == model_scale &
-    model_specs$Feature_selection_employed == Feature_selection_employed &
-    model_specs$Balance_adjustment == Balance_adjustment, ]
+  predict_model_specs <- model_specs[
+    model_specs$Model_type == model_type &
+      model_specs$model_scale == model_scale &
+      model_specs$Feature_selection_employed == feature_selection_employed &
+      model_specs$Balance_adjustment == balance_adjustment,
+  ]
+
 
   # change completion values to 'N'
-  Predict_model_specs$Modelling_completed <- "N"
+  predict_model_specs$Modelling_completed <- "N"
 
   # Print a warning that not all data periods are included in the predict model specs table
-  if (all(data_periods %in% unique(Predict_model_specs$data_period_name)) == FALSE) {
+  if (all(data_periods %in% unique(predict_model_specs$data_period_name)) == FALSE) {
     cat(paste0(
       "Warning the data period/s: ",
-      paste(data_periods[which(!(data_periods %in% unique(Predict_model_specs$data_period_name)))], sep = ","),
+      paste(
+        data_periods[which(!(data_periods %in% unique(predict_model_specs$data_period_name)))],
+        sep = ","
+      ),
       " do not have models of the desired specification completed for them
            (i.e. Errors may have occured in model fitting or evaluation,
            please check before continuing"
@@ -45,20 +61,24 @@ lulcc.finalisemodelspecifications <- function(model_specs_path, param_grid_path)
   }
 
   # save prediction model specs
-  openxlsx::write.xlsx(Predict_model_specs, file = "Tools/Predict_model_specs.xlsx")
+  openxlsx::write.xlsx(predict_model_specs, file = "tools/predict_model_specs.xlsx")
 
   # Load parameter grid
-  param_grid <- read_excel(param_grid_path, sheet = Model_type)
+  param_grid <- readxl::read_excel(param_grid_path, sheet = model_type)
 
   # create a duplicate parameter grid for prediction
-  Predict_param_grid <- data.frame(matrix(nrow = nrow(param_grid), ncol = ncol(param_grid)))
-  colnames(Predict_param_grid) <- colnames(param_grid)
+  predict_param_grid <- data.frame(matrix(nrow = nrow(param_grid), ncol = ncol(param_grid)))
+  colnames(predict_param_grid) <- colnames(param_grid)
 
   # fill grid
   for (i in colnames(param_grid)) {
-    Predict_param_grid[[i]] <- readline(prompt = paste0("Enter value for parameter ", i, ": "))
+    predict_param_grid[[i]] <- readline(prompt = paste0("Enter value for parameter ", i, ": "))
   }
 
   # save prediction parameter grid
-  openxlsx::write.xlsx(Predict_param_grid, file = "Tools/Predict_param-grid.xlsx", sheetName = Model_type)
+  openxlsx::write.xlsx(
+    predict_param_grid,
+    file = "tools/predict_param-grid.xlsx",
+    sheetName = model_type
+  )
 }
