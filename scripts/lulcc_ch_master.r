60d109e83fb0ca90861c49160124c8c834795b5d
mmyrte <24587121+mmyrte@users.noreply.github.com>
2025-02-07 18:58:20 +0100

    call functions without namespace

--- scripts/lulcc_ch_master.r
+++ scripts/lulcc_ch_master.r
@@ -13,9 +13,9 @@
 #############################################################################
 
 # presumed working dir: root of the repo
-devtools::load_all("lulccfunspkg")
+devtools::load_all()
 
-config <- lulccfunspkg::get_config()
+config <- get_config()
 
 #' Download and unpack data
 #' FIXME this all-in-one download might be the wrong strategy:
@@ -23,56 +23,56 @@ config <- lulccfunspkg::get_config()
 #' - the zenodo file is corrupted and needs to be replaced
 #' - the zenodo file is pretty large, which increases risk of corruptinon during
 #'   download. might want to segment into multiple smaller chunks
-#' lulccfunspkg::fetch_zenodo_predictors()
+#' fetch_zenodo_predictors()
 
 # A- Prepare LULC/region data
 # Prepare LULC data layers
-lulccfunspkg::lulc_data_prep(config = config)
+lulc_data_prep(config = config)
 
 #' Prepare raster of Swiss Bioregions
 #' TODO why is this prepared? none of the produced files seem to be referenced
-#' lulccfunspkg::region_prep(config = config)
+#' region_prep(config = config)
 
 # B- Prepare predictor data
 # Prepare suitability and accessibility predictors
-lulccfunspkg::calibration_predictor_prep(config = config)
+calibration_predictor_prep(config = config)
 
 # C- Identify LULC transitions and create transition datasets
-lulccfunspkg::transition_identification(config = config)
+transition_identification(config = config)
 
 # D- Create transition datasets
-lulccfunspkg::transition_dataset_prep(config = config)
+transition_dataset_prep(config = config)
 
 # E- Predictor variable selection on LULCC transition datasets
-lulccfunspkg::transition_feature_selection(config = config)
+transition_feature_selection(config = config)
 
 # F- Statistical modelling of LULCC transition datasets
 # G- Summarizing model validation results
-lulccfunspkg::transition_modelling(config = config)
+transition_modelling(config = config)
 
 # adjust contents of model_specs table to only optimal specifcations
-lulccfunspkg::lulcc.finalisemodelspecifications(
+lulcc.finalisemodelspecifications(
   model_specs_path = model_specs_path,
   param_grid_path = param_grid_path
 )
 
 # H- Re-fitting optimal model specifications on full data
-lulccfunspkg::trans_model_finalization(config = config)
+trans_model_finalization(config = config)
 
 # I- Prepare data for deterministic transitions (e.g glacier -> Non-glacier)
-lulccfunspkg::deterministic_trans_prep(config = config)
+deterministic_trans_prep(config = config)
 
 # I- Prepare tables of transition rates for scenarios
-lulccfunspkg::simulation_trans_tables_prep(config = config)
+simulation_trans_tables_prep(config = config)
 
 # J- Prepare predictor data for scenarios
-lulccfunspkg::simulation_predictor_prep(config = config)
+simulation_predictor_prep(config = config)
 
 ## K- Calibrate allocation parameters for Dinamica
-lulccfunspkg::calibrate_allocation_parameters(config = config)
+calibrate_allocation_parameters(config = config)
 
 # L- Prepare scenario specific spatial interventions
-lulccfunspkg::spatial_interventions_prep(config = config)
+spatial_interventions_prep(config = config)
 
 # M- Run Dinamica simulations over scenarios
-lulccfunspkg::run_dinamica_sims(config = config)
+run_dinamica_sims(config = config)
