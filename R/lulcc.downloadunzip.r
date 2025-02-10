34d33e1cb1260fa6d4a1fa947651f25e0402aaa9
mmyrte <24587121+mmyrte@users.noreply.github.com>
2025-02-10 16:45:05 +0100

    lulcc.downloadunzip was misbehaving with DOS encoded filenames
    so it's now using the getOption("unzip") to retrieve the path of unzip
    on unixen.

--- R/lulcc.downloadunzip.r
+++ R/lulcc.downloadunzip.r
@@ -5,24 +5,27 @@
 #' @param url character string url to download folder from
 #'   produced by function lulcc.evalfeatureselection
 #' @param save_path character string path of directory to save unzipped files in
+#' @param filename defaults to the `basename(url)`
 #'
 #' @author Ben Black
 #' @export
 
-lulcc.downloadunzip <- function(url, save_dir) {
+lulcc.downloadunzip <- function(url, save_dir, filename = basename(url)) {
   # create dir
-  dir.create(save_dir, recursive = TRUE)
-
-  # get file base name
-  file <- basename(url)
+  ensure_dir(save_dir)
 
   # if file is zipped then create temp dir and extract
-  if (grepl(".zip", url) == TRUE) {
+  if (grepl("\\.zip", filename)) {
     tmpdir <- tempdir()
-    download.file(url, paste0(tmpdir, "/", file))
-    unzip(paste0(tmpdir, "/", file), exdir = save_dir) # unzip folder, saving to new dir
+    download.file(url, paste0(tmpdir, "/", filename), mode = "wb")
+    unzip(
+      zipfile = paste0(tmpdir, "/", filename),
+      exdir = save_dir,
+      # system unzip on *nix; more robust against encoding issues
+      unzip = getOption("unzip")
+    )
     unlink(tmpdir) # remove temp dir
   } else { # non-zipped just download
-    download.file(url, paste0(save_dir, "/", file), mode = "wb")
+    download.file(url, paste0(save_dir, "/", filename), mode = "wb")
   }
 }
