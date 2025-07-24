67b2ff05f26e3273c7cdf9d66be8dc7646d22d07
mmyrte <24587121+mmyrte@users.noreply.github.com>
2025-07-24 12:49:09 +0200

    clean dinamica_load_trans_table

--- R/dinamica_load_trans_table.r
+++ R/dinamica_load_trans_table.r
@@ -1,66 +1,51 @@
-#############################################################################
-## Dinamica_load_trans_matrix: Load Scenario/time specific transition matrix
-## and update to reflect any deterministic transitions
-## Date: 03-05-2023
-## Author: Ben Black
-#############################################################################
-dinamica_load_trans_matrix <- function() {
-  # A- Preparation ####
+#' dinamica_load_trans_table
+#'
+#' Loads a scenario and year-specific transition matrix for a simulation, and updates it
+#' to reflect any deterministic transitions (such as glacier transitions).
+#'
+#' @param simulation_id Integer. The ID of the simulation to load parameters for.
+#' @param year Integer. The year for which to load the transition matrix.
+#' @param config List. Configuration settings, defaults to the result of [get_config()].
+#'
+#' @return A data frame containing the transition table ("From*" (int), "To*" (int),
+#' "Rate" (float))
+#'
+#' @author Ben Black
+#' @export
 
-  # wpath <- getwd()
-  setwd(wpath)
-
-  # Vector packages for loading
-  packs <- c(
-    "data.table", "raster", "tidyverse", "stringr", "readr", "xlsx",
-    "Dinamica", "readxl"
-  )
-
-  new.packs <- packs[!(packs %in% installed.packages()[, "Package"])]
-  if (length(new.packs)) install.packages(new.packs)
-
-  # Load required packages
-  invisible(lapply(packs, require, character.only = TRUE))
-
-  # load table of simulations
-  control_table <- read.csv(Control_table_path)[simulation_num, ]
-
-  # Vector name of Scenario to be tested as string or numeric (i.e. "BAU" etc.)
-  scenario_id <- Simulation_table$scenario_id.string
-
-  # Vector ID for this run of the scenario (e.g V1)
-  simulation_id <- control_table$simulation_id.string
-
-  # Define model_mode: Calibration or Simulation
-  model_mode <- Simulation_table$model_mode.string
-
-  # B- Load Transition rate table and update to reflect any deterministic transitions ####
+dinamica_load_trans_table <- function(
+    simulation_id,
+    year,
+    config = get_config()) {
+  params <- get_simulation_params(simulation_id = simulation_id)
 
   # use scenario ID to grab folder path of scenario specific transition tables
-  Scenario_trans_table_dir <- stringr::str_remove(
-    list.files(
-      trans_rate_table_dir,
-      pattern = scenario_id,
-      full.names = TRUE
-    ),
-    paste0(wpath, "/"),
+  scenario_trans_table_dir <- list.files(
+    config[["trans_rate_table_dir"]],
+    pattern = params[["scenario_id.string"]],
+    full.names = TRUE
   )
 
   # Use folder path to create a generic scenario specific transition matrix file path
-  Scenario_trans_table_file <- file.path(
-    Scenario_trans_table_dir,
+  scenario_trans_table_file <- file.path(
+    scenario_trans_table_dir,
     paste0(
-      scenario_id, "_", "trans_table", "_", Simulation_year, ".csv"
+      params[["scenario_id.string"]], "_trans_table_", year, ".csv"
     )
   )
 
   # load the table
-  Trans_table <- read_csv(Scenario_trans_table_file)
+  trans_table <- readr::read_csv(scenario_trans_table_file, col_types = "iid")
 
-  # if statement to remove transitions if they are being implemented deterministicly
-  if (grepl("simulation", model_mode, ignore.case = TRUE) &
-    grepl("Y", Simulation_table$deterministic_trans.string, ignore.case = TRUE)) {
+  # remove glacier transition (implemented deterministically)
+  if (
+    grepl("simulation", params[["model_mode.string"]], ignore.case = TRUE) &&
+      grepl("Y", params[["deterministic_trans.string"]], ignore.case = TRUE)
+  ) {
+    # TODO remove hard-coded assumption that glacier == 19
     # remove transitions with initial class == glacier
-    Trans_table <- Trans_table[Trans_table$`From*` != 19, ]
-  } # close if statement
+    trans_table <- trans_table[trans_table$`From*` != 19, ]
+  }
+
+  return(trans_table)
 }
